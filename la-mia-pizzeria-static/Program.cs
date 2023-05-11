a4ff7a02424b45008c659a17360516e6062ab74e
KellerMarika <marikakeller92@gmail.com>
2023-05-11 16:34:00 +0200

    f5 pre dotnet tool install -g dotnet-aspnet-codegenerator

--- la-mia-pizzeria-static/Program.cs
+++ la-mia-pizzeria-static/Program.cs
@@ -49,6 +49,7 @@ namespace la_mia_pizzeria_static
 
             app.UseRouting();
 
+            app.UseAuthentication();
             app.UseAuthorization();
 
             app.MapControllerRoute(
